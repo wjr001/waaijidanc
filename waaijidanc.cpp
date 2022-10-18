8f56f5f40ebe3660e688cf0ff71577cb1e4dc967
NUC12 <wjracr@outlook.com>
2022-10-18 17:16:47 +0800

    v0.1.0

--- waaijidanc.cpp
+++ waaijidanc.cpp
@@ -1,13 +1,9 @@
-﻿
-#include <iostream>
-#include <winsock2.h> //该头文件需在windows.h之前
+﻿#include <iostream>
 #include <windows.h>
 #include <fstream>
 #include <string>
 #include <sstream>
 using namespace std;
-#include <json/json.h>
-#pragma execution_character_set("utf-8")
 #pragma comment(lib, "ws2_32.lib")
 #pragma warning(disable : 4996)
 #pragma warning(disable : 6387)

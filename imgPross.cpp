d6490b9687e97ce02383a3f71a681afd4059431d
NUC12 <wjracr@outlook.com>
2022-10-20 01:24:24 +0800

    v0.2.0

--- imgPross.cpp
+++ imgPross.cpp
@@ -6,7 +6,7 @@ using namespace cv;
 #pragma warning(disable : 4996)
 #pragma warning(disable : 6387)
 
-bool imgPross()
+bool imgPross(const char* item)
 {
 	Mat img = imread("./orgImg.png", 0);
 	if (img.empty())
@@ -16,14 +16,14 @@ bool imgPross()
 	//imshow("test", img);
 	//waitKey(0);
 	Mat poss_img;
-	threshold(img, poss_img, 80, 255, THRESH_BINARY);
+	threshold(img, poss_img, 90, 255, THRESH_BINARY);
 	//imshow("test", poss_img);
 	//waitKey(0);
 	imwrite("after_pros.png", poss_img);
 	char* fileDir = (char*)malloc(260);
 	getcwd(fileDir, 260);
 	char* output = (char*)malloc(1024);
-	sprintf_s(output, 1024, "tesseract %s/after_pros.png %s/text -l eng", fileDir, fileDir);
+	sprintf_s(output, 1024, "tesseract %s/after_pros.png %s/text -l %s", fileDir, fileDir,item);
 	cout << output;
 	system(output);
 	free(fileDir);
--- main.cpp
+++ main.cpp
@@ -12,22 +12,38 @@ using namespace std;
 
 //bool HttpGetReq(char* vocabulary);
 bool analysis(char* vocabulary);
-bool imgPross();
+bool imgPross(const char* item);
 
 int main()
 {
 	//system("chcp 65001"); 
-	imgPross();
+	imgPross("eng");
 	ifstream text;
 	text.open("text.txt", ios::in);
-	char* textTemp=(char*)malloc(64);
-	text >> textTemp;
+	string eng[2];
+	text >> eng[0];
+	while (!text.eof())
+	{
+		eng[1] = eng[0];
+		text >> eng[0];
+		if (((eng[0].compare("A"))==0)|| ((eng[0].compare("A.")) == 0))
+		{
+			if (isalpha((eng[1])[0]))
+			{
+				analysis((char*)eng[1].c_str());
+			}
+			break;
+		}
+	}
+	
+	
+	/*
 	while (!text.eof())
 	{
 		text >> textTemp;
-		analysis((char*)textTemp);
+		
 	}
-	
+	*/
 	return 0;
 
 }
